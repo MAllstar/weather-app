a2c53d1d93de02f28234a2e324335e8116cb63d4
yuch256 <3022489608@qq.com>
2019-12-10 19:28:05 +0800

    map ok

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -9,38 +9,93 @@ import {
   StatusBar,
   Button,
   Dimensions,
-  NativeModules
 } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
-import { BaiduMapManager, MapView, MapTypes, Geolocation, Overlay, MapApp } from 'react-native-baidu-map';
+import { BaiduMapManager, MapView, MapTypes, Geolocation, Overlay } from 'react-native-baidu-map';
+
+BaiduMapManager.initSDK("C3CjoByz3moD15i4O3Dr1ymqkE8LmYg6");
+const { height, width } = Dimensions.get('window');
 
 class Map extends Component {
+  state = {
+    location: {},
+    // center: { longitude: 113.950453, latitude: 22.546045 },
+    center: { longitude: 120.019152, latitude: 30.295559 },
+  };
+
+  componentDidMount() {
+    this.getCurrentPosition()
+  };
+
+  getCurrentPosition() {                        // 获取当前定位
+    Geolocation.getCurrentPosition()
+      .then((data) => {
+        this.setState({ location: data, center: data });
+      });
+  };
+
+  handleChangeLocation = (location) => {
+    let SZDX = { longitude: 113.950453, latitude: 22.544045 };
+    let HZNU = { longitude: 120.019152, latitude: 30.295559 };
+    let London = { longitude: -0.171877, latitude: 51.496431 };
+    if (location === '深圳大学') this.setState({ center: SZDX, location: SZDX });
+    else if (location === '杭州师范大学') this.setState({ center: HZNU, location: HZNU });
+    else if (location === '伦敦') this.setState({ center: London, location: London });
+  };
+
   render() {
+    const { location, center } = this.state;
     return (
-      <View style={styles.container}>
-        <Text>Map</Text>
-      </View>
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <ScrollView
+            // contentInsetAdjustmentBehavior="automatic"
+            style={styles.scrollView}>
+            <View style={styles.body}>
+              <MapView
+                showsUserLocation={true}
+                locationData={location}
+                width={width}
+                height={height * 0.65}
+                zoom={18}
+                trafficEnabled={true}
+                zoomControlsVisible={true}
+                mapType={MapTypes.NORMAL}
+                center={center}
+              >
+              </MapView>
+              <View style={styles.buttonGroup}>
+                <Button onPress={() => this.handleChangeLocation('深圳大学')} title="深圳大学" />
+                <Button onPress={() => this.handleChangeLocation('杭州师范大学')} title="杭州师范大学" />
+                <Button onPress={() => this.handleChangeLocation('伦敦')} title="伦敦" />
+              </View>
+              <View style={styles.location}>
+                <Text>{`latitude: ${location.latitude}, longitude: ${location.longitude}`}</Text>
+              </View>
+            </View>
+          </ScrollView>
+        </SafeAreaView>
+      </>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    justifyContent: 'flex-end',
-    alignItems: 'center'
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
-  map: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0
-  }
+  location: {
+    paddingLeft: 16,
+  },
+  buttonGroup: {
+    padding: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
 });
 
 export default MapStack = createStackNavigator({
--- src/pages/Weather.js
+++ src/pages/Weather.js
@@ -1,19 +1,52 @@
 import React, { Component } from 'react';
 import {
-  View,
   Text,
+  StyleSheet,
+  View,
 } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
+import { Container, Header, Tab, Tabs, TabHeading } from 'native-base';
 
 class Weather extends Component {
+  state = {
+    active: false
+  };
+
+  handleChangeTabsActive = () => {
+    console.log(1)
+    this.setState({ active: !this.state.active });
+  };
+
   render() {
+    let { active } = this.state;
     return (
-      <View>
-        <Text>Weather</Text>
-      </View>
+      <Container>
+        <Tabs>
+          <Tab heading={
+            <TabHeading style={{ backgroundColor: active ? '#fff' : 'tomato' }}>
+              <Text>今日</Text>
+            </TabHeading>
+          }>
+            <Text>123</Text>
+          </Tab>
+          <Tab onPress={this.handleChangeTabsActive} heading={
+            <View onPress={this.handleChangeTabsActive}>
+              <TabHeading
+                style={{ backgroundColor: active ? 'tomato' : '#fff' }}>
+                <Text>推荐</Text>
+              </TabHeading>
+            </View>
+          }>
+            <Text>321</Text>
+          </Tab>
+        </Tabs>
+      </Container>
     );
   }
 }
+
+const styles = StyleSheet.create({
+});
 
 export default WeatherStack = createStackNavigator({
   Weather: {
