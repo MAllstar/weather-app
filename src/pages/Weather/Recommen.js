461022b7309a0bcd0946a741ce6da0f484cea39f
yuch256 <3022489608@qq.com>
2019-12-13 22:48:04 +0800

    天气 -> 推荐

--- src/pages/Weather/Recommen.js
+++ src/pages/Weather/Recommen.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import {
   View,
-  Image,
+  Text,
+  ScrollView,
 } from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
 
 export default class Recommen extends Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'space-evenly', alignItems: 'center' }}>
-        <Image style={{ width: 150, height: 150 }} source={{ uri: 'https://facebook.github.io/react-native/img/tiny_logo.png' }} />
-        <Image style={{ width: 150, height: 150 }} source={{ uri: 'https://images.freeimages.com/images/large-previews/b10/can-t-look-over-1312680.jpg' }} />
+      <View style={{ flex: 1 }}>
+        <ScrollView style={{ paddingHorizontal: 10, paddingVertical: 10 }}>
+          <View style={{ width: '100%', alignItems: 'center' }}>
+            <QRCode
+              value="http://www.hznu.edu.cn"
+              size={200}
+            />
+          </View>
+          <View style={{ marginTop: 15 }}>
+            <Text>温馨提示：昼夜温差较大，较易发生感冒，请适当增减衣服。体质较弱的朋友请注意防护。</Text>
+          </View>
+        </ScrollView>
       </View>
     );
   }
