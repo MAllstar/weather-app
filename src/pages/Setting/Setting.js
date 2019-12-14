77345aee5ae8c0180d18f51508fbd19426fafdd4
yuch256 <3022489608@qq.com>
2019-12-14 23:39:38 +0800

    react-native-sqlite-storage error...

--- src/pages/Setting/Setting.js
+++ src/pages/Setting/Setting.js
@@ -6,11 +6,11 @@ import HistorySet from './HistorySet';
 import SignOut from './SignOut';
 
 const MyDrawerNavigator = createDrawerNavigator({
-  '用户资料': UserInfo,
+  '用户信息': UserInfo,
   '历史页面': HistorySet,
   '登录设置': SignOut,
 }, {
-  initialRouteName: '用户资料',
+  initialRouteName: '用户信息',
   drawerPosition: 'left',
   drawerType: 'front',
   hideStatusBar: true,
