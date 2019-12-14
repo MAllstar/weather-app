35acaa959d034eef88250583f61b10731c21ee5e
yuch256 <3022489608@qq.com>
2019-12-15 00:07:00 +0800

    SQLite connection succeeded! niiiiice!

--- src/pages/History/History.js
+++ src/pages/History/History.js
@@ -5,15 +5,36 @@ import {
   Text,
 } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
+import SQLite from 'react-native-sqlite-storage';
 
 class History extends Component {
-  state = {
-    historyDay: 7
-  };
+  constructor(props) {
+    super(props);
+    const db = SQLite.openDatabase(
+      {
+        name: 'WeatherApp.db',
+        location: 'default',
+        createFromLocation: '~www/WeatherApp.db'
+      },
+      () => console.log('SQLite connection succeeded!'),
+      err => console.log(err)
+    );
+    this.state = {
+      db,
+      historyDay: 7
+    }
+  }
 
   async componentDidMount() {
     let historyDay = await AsyncStorage.getItem('historyDay')
     if (historyDay) this.setState({ historyDay })
+    let { db } = this.state;
+    db.transaction(tx => {
+      tx.executeSql('SELECT * FROM history;', [], (tx, result) => {
+        console.log("Query completed");
+        console.log(result)
+      })
+    })
   }
 
   render() {
