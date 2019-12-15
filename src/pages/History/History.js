ad881af211a9335fe16be68b9216c9c723b9495d
yuch256 <3022489608@qq.com>
2019-12-15 18:55:26 +0800

    。。。

---
+++ src/pages/History/History.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import {
+  View,
+  AsyncStorage,
+  Text,
+} from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+
+class History extends Component {
+  state = {
+    historyDay: 7
+  };
+
+  async componentDidMount() {
+    let historyDay = await AsyncStorage.getItem('historyDay')
+    if (historyDay) this.setState({ historyDay })
+  }
+
+  render() {
+    return (
+      <View><Text>{this.state.historyDay}</Text></View>
+    );
+  }
+}
+
+import HeaderRightInfo from '../../components/common/HeaderRightInfo';
+
+export default HistoryStack = createStackNavigator({
+  History: {
+    screen: History,
+    navigationOptions: ({ navigation }) => ({
+      title: '历史',
+      headerRight: <HeaderRightInfo />
+    })
+  }
+});
